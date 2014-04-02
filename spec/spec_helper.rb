b4a1a3acb48f2bbb1d254756b7addcfe3c19555b
Chris Armstrong <chris@opdemand.com>
2014-04-02 15:14:13 -0700

    Removes 'service: command not found' errors when testing on non-Ubuntu

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,18 @@ require 'chefspec/berkshelf'
 # See https://github.com/sethvargo/chefspec/issues/393
 # ChefSpec::Coverage.start!
 
+# Prevent system calls from getting run on our local machine
+# lxc cookbook does this: https://github.com/hw-cookbooks/lxc/blob/master/recipes/default.rb#L47
+# TODO Figure out a better way to do this - couldn't get stubbing to work (see below)
+module Kernel
+  def system(cmd)
+    return true
+  end
+end
+
 # Specify defaults -- these can be overridden
 RSpec.configure do |config|
+
   config.log_level = :error # necessary to suppress all the WARNs for Chef resource cloning
   config.platform = 'ubuntu'
   config.version = '12.04'
@@ -16,5 +26,7 @@ RSpec.configure do |config|
     require 'etcd'
     Etcd.stub(:client).and_return(etcd_stub)
     allow(etcd_stub).to receive(:get).and_raise(Net::HTTPFatalError.new('stub', 503))
+    # Also stub system, because it's called in the lxc cookbook: https://github.com/hw-cookbooks/lxc/blob/9f113a34c0535c3e474b03fdd45af886c2132d3c/recipes/default.rb#L47
+    #allow(Kernel).to receive(:system).and_return(true)
   end
 end
