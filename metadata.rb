77a1d8487dd6203cb7c5ab89b67a4db8110aa3f9
Chris Armstrong <chris@opdemand.com>
2014-04-03 12:52:10 -0700

    Switch master to v0.8.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chefs@opdemand.com'
 license          'Apache 2.0'
 description      'Installs/Configures components of the Deis PaaS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.7.0'
+version          '0.8.0'
 
 depends          'apt',       '~> 2.3'
 depends          'docker',    '~> 0.33'
