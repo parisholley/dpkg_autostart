d3f36f21452cb723fae5c655598fce9d607bf747
Brian Flad <bflad417@gmail.com>
2014-04-06 12:10:43 -0400

    ChefSpec Runners have been added to Docker cookbook
    https://github.com/bflad/chef-docker/commit/2d3390bc000a6e6919a498582cb2592100a8736f

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -7,9 +7,4 @@ if defined?(ChefSpec)
   def remove_sudo(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:sudo, :remove, resource_name)
   end
-
-  # These should live in the docker cookbook: https://github.com/bflad/chef-docker/pull/112
-  ChefSpec::Runner.define_runner_method(:docker_container)
-  ChefSpec::Runner.define_runner_method(:docker_image)
-  ChefSpec::Runner.define_runner_method(:docker_registry)
 end
